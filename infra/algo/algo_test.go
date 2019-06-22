6d02c71bbb1ba96649efba6f6ed9d262dcc73093
HackFun <hackfeng624@gmail.com>
2019-06-22 15:37:36 +0800

    goconvey

---
+++ infra/algo/algo_test.go
@@ -0,0 +1,36 @@
+package algo
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestSimpleRand(t *testing.T) {
+	ForTest("简单随机算法", t, SimpleRand)
+}
+
+func TestBeforeShuffle(t *testing.T) {
+	ForTest("先洗牌算法", t, BeforeShuffle)
+}
+
+func TestDoubleRandom(t *testing.T) {
+	ForTest("二次随机算法", t, DoubleRandom)
+}
+
+func TestDoubleAverage(t *testing.T) {
+	ForTest("二倍随机算法", t, DoubleAverage)
+}
+
+func ForTest(message string, t *testing.T, fn func(count, amount int64) int64) {
+	count, amount := int64(10), int64(10000)
+	remain := amount
+	sum := int64(0)
+	for i := int64(0); i < count; i++ {
+		x := fn(count-i, remain)
+		remain -= x
+		sum += x
+	}
+	Convey(message, t, func() {
+		So(sum, ShouldEqual, amount)
+	})
+}
